75698ba94b0aec84422953575a4ab935a2e61fd1
darksdieforce <mayuemaxiao@qq.com>
2020-07-07 00:03:58 +0800

    登录页面的第一次修改

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/css/icon.css'
 
 Vue.config.productionTip = false
 
